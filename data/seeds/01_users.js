ed35873bfdd963766b6bf12723c79253bee0920a
Ryan Ishimaru <rishimaru23@gmail.com>
2021-08-20 12:08:11 -0700

    mvp

--- data/seeds/01_users.js
+++ data/seeds/01_users.js
@@ -6,7 +6,7 @@ exports.seed = function(knex) {
       // Inserts seed entries
       return knex('users').insert([
         {id: 1, username:'user 1', password: 'rowVa$2a$08$xgNYEvHsoJm0Kfbn7lxZiODfzAX7eZMACg4Ml.JpmjnObX1DG7jRe'},
-        {id: 2, username:'user 2', password: '$2a$08$LADeY4BUnCtZpEXfPjaxoeZGtNhE6yQbJ/M6D.uP0X/RCoJSFofni'}
+        {id: 2, username:'bonto', password: '$2a$08$LADeY4BUnCtZpEXfPjaxoeZGtNhE6yQbJ/M6D.uP0X/RCoJSFofni'}
       ]);
     });
 };
